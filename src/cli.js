79b03b140818e955584e42e5d93a8a50438f8355
Oleg Tsyganov <TsyganovDev@gmail.com>
2021-08-30 12:45:56 +0400

    правки

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
+export const username = () => {
   const name = readlineSync.question('May I have your name?: ');
   console.log(`'Hello, ${name}!`);
 };
