9f580acb1621d6e0556370cc4b350e6ac230cfe0
Oleg Tsyganov <TsyganovDev@gmail.com>
2021-08-30 21:18:23 +0400

    правки

---
+++ src/common.js
@@ -0,0 +1,6 @@
+const printText = (str) => console.log(str);
+const getRandomNumber = () => Math.floor(Math.random() * 100);
+
+export {
+  printText, getRandomNumber,
+};
--- src/event-game.js
+++ src/event-game.js
@@ -1,48 +1,30 @@
 import readlineSync from 'readline-sync';
+import { getRandomNumber, printText } from './common.js';
 
 export default () => {
   const name = readlineSync.question('May I have your name?: ');
-  console.log(`'Hello, ${name}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  const getRandomNumber = () => Math.floor(Math.random() * 100);
+  printText(`'Hello, ${name}!`);
+  printText('Answer "yes" if the number is even, otherwise answer "no".');
   for (let i = 1; i <= 3; i += 1) {
     const number = getRandomNumber();
-    console.log(`Question: ${number}`);
+    printText(`Question: ${number}`);
     const youAnswer = readlineSync.question('You answer: ');
     if (number % 2 === 0) {
       if (youAnswer === 'yes') {
-        console.log('Correct!');
+        printText('Correct!');
       } else {
-        console.log(`'${youAnswer}' is wrong answer ;(. Correct answer was 'yes'.`);
-        return (
-          console.log(`Let's try again, ${name}!`)
-        );
+        printText(`'${youAnswer}' is wrong answer ;(. Correct answer was 'yes'.`);
+        return printText(`Let's try again, ${name}!`);
       }
     }
     if (number % 2 !== 0) {
       if (youAnswer === 'no') {
-        console.log('Correct!');
+        printText('Correct!');
       } else {
         console.log(`'${youAnswer}' is wrong answer ;(. Correct answer was 'no'.`);
-        return (
-          console.log(`Let's try again, ${name}!`)
-        );
+        return printText(`Let's try again, ${name}!`);
       }
     }
-    // if (number % 2 === 0 && youAnswer === 'yes') {
-    //   console.log('Correct!');
-    // } else {
-    //   console.log(`'${youAnswer}' is wrong answer ;(. Correct answer was 'yes'.`);
-    //   return `Let's try again, ${name}!`;
-    // }
-    // if (number % 2 !== 0 && youAnswer === 'no') {
-    //   console.log('Correct!');
-    // } else {
-    //   console.log(`'${youAnswer}' is wrong answer ;(. Correct answer was 'no'.`);
-    //   return `Let's try again, ${name}!`;
-    // }
   }
-  return (
-    console.log(`Congratulations, ${name}!`)
-  );
+  return printText(`Congratulations, ${name}!`);
 };
